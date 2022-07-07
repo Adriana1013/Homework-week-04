f1ba34bbce7c8e63dcd2eb282b0ffa4c50a97740
Adriana Villegas <villegas.a102@gmail.com>
2022-07-07 17:16:56 -0500

    working timer but still figuring out the quiz section

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Homework-week-04
+
+Here are a few screenshots of what the challenge will look like: 
+<img width="1205" alt="Screen Shot 2022-07-06 at 8 35 29 PM" src="https://user-images.githubusercontent.com/106636876/177879970-ba51c064-a0d7-4f00-b8a1-2a62b735471b.png">
+<img width="805" alt="Screen Shot 2022-07-06 at 8 37 53 PM" src="https://user-images.githubusercontent.com/106636876/177879985-3be067ea-b8d6-47e6-83a0-3acfba40aa47.png">
+<img width="813" alt="Screen Shot 2022-07-06 at 8 38 12 PM" src="https://user-images.githubusercontent.com/106636876/177880063-08b5c18b-de8c-49d7-bd64-d74f9a04923c.png">
+
--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 var startBtn = document.querySelector("#start");
+var secondsRemaining = 75; 
+var countDown = document.getElementById("timer");
+// var buttonOne = document.querySelector("A");
+// var buttonTwo = document.querySelector("B");
+// var buttonThree = document.querySelector("C");
+// var buttonFour = document.querySelectorAll('D');
+
+function startTheTimer() {
+  var timerInterval = setInterval(function() {
+  secondsRemaining--; 
+  countDown.textContent = " Time: " + secondsRemaining;
+
+  if (secondsRemaining === 0) {
+    clearInterval(timerInterval);
+    sendMessage();
+  }
+  }, 1000);        
+} 
+
+function sendMessage() {
+  countDown.textContent = "Time is Up";
+}
+
+// function quiz [{
+//     question: "Where is the script tag placed on the HTML page?",
+//     answers:
+//       A: "In the head tag",
+//       B: "Before the closing body tag",  
+//       C: "After the closing html tag", 
+//       D: "inside of a div tag"     
+//     correctAnswer: "B"
+// },
+// ];
+
+// When I click the start button it will start the timer
+startBtn.addEventListener("click", startTheTimer)
