e2f487362a0ccaf6d14ac0ee9398cad467db3ab6
Adriana Villegas <villegas.a102@gmail.com>
2022-07-07 23:57:59 -0500

    final submission

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Homework-week-04
+Web APIs Challenge: Code Quiz
+
+User Story: 
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+Acceptance Criteria:
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
 
 Here are a few screenshots of what the challenge will look like: 
 <img width="1205" alt="Screen Shot 2022-07-06 at 8 35 29 PM" src="https://user-images.githubusercontent.com/106636876/177879970-ba51c064-a0d7-4f00-b8a1-2a62b735471b.png">
 <img width="805" alt="Screen Shot 2022-07-06 at 8 37 53 PM" src="https://user-images.githubusercontent.com/106636876/177879985-3be067ea-b8d6-47e6-83a0-3acfba40aa47.png">
 <img width="813" alt="Screen Shot 2022-07-06 at 8 38 12 PM" src="https://user-images.githubusercontent.com/106636876/177880063-08b5c18b-de8c-49d7-bd64-d74f9a04923c.png">
 
+Links:
+https://github.com/Adriana1013/Homework-week-04
+https://adriana1013.github.io/Homework-week-04/
--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var startBtn = document.querySelector("#start");
+var startBtn = document.querySelector("start");
 var secondsRemaining = 75; 
 var countDown = document.getElementById("timer");
-// var buttonOne = document.querySelector("A");
-// var buttonTwo = document.querySelector("B");
-// var buttonThree = document.querySelector("C");
-// var buttonFour = document.querySelectorAll('D');
+var questionBox = document.querySelector("questionTextBox")
+var buttonOne = document.querySelector("btn2");
+var buttonTwo = document.querySelector("btn2");
+var buttonThree = document.querySelector("btn2");
+var buttonFour = document.querySelector('btn2');
 
+
+function beginQuiz() {
+// questionBox.textContent = "Where is the script tag placed on the HTML page?";
+//   buttonOne.textContent = A: "In the head tag",
+//   buttonTwo.textContent = B: "Before the closing body tag",  
+//   buttonThree.textContent = C: "After the closing html tag", 
+//   buttonFour.textContent = D: "inside of a div tag"     
+}; 
+
+// The timer is set and will stop when time is up or all questions are answered
 function startTheTimer() {
   var timerInterval = setInterval(function() {
   secondsRemaining--; 
@@ -15,23 +26,14 @@ function startTheTimer() {
     clearInterval(timerInterval);
     sendMessage();
   }
-  }, 1000);        
+  }, 1000);      
 } 
 
 function sendMessage() {
   countDown.textContent = "Time is Up";
 }
 
-// function quiz [{
-//     question: "Where is the script tag placed on the HTML page?",
-//     answers:
-//       A: "In the head tag",
-//       B: "Before the closing body tag",  
-//       C: "After the closing html tag", 
-//       D: "inside of a div tag"     
-//     correctAnswer: "B"
-// },
-// ];
+startTheTimer();
 
-// When I click the start button it will start the timer
-startBtn.addEventListener("click", startTheTimer)
+// when I click the button the quiz will appear
+startBtn.addEventListener("click", beginQuiz);
