e7d3dd770e9c4991eb958995617da85ea49460f6
Adriana Villegas <villegas.a102@gmail.com>
2022-11-27 23:59:23 -0600

    using getelementbyid to fix bugs

--- script.js
+++ script.js
@@ -1,20 +1,107 @@
-var startBtn = document.querySelector("start");
-var secondsRemaining = 75; 
+var startBtn = document.getElementById("start");
+var askQuestions = document.getElementById("questionTextBox");
+var introPage = document.getElementById("intro")
+
+var questionSection = document.getElementById('question');
+var answerSection = document.getElementById('answers');
+
+let allQuestions, currentQuestionAsked
+
+var secondsRemaining = 45; 
 var countDown = document.getElementById("timer");
-var questionBox = document.querySelector("questionTextBox")
-var buttonOne = document.querySelector("btn2");
-var buttonTwo = document.querySelector("btn2");
-var buttonThree = document.querySelector("btn2");
-var buttonFour = document.querySelector('btn2');
 
+// begins the quiz game
+function playQuiz() {
+console.log('started')
+startBtn.remove("hide")
+askQuestions.classList.remove("hide")
+introPage.remove("hide")
+allQuestions = quizQuestions.sort(() => Math.random() -.5)
+currentQuestionAsked = 0
+continueQuiz();
+startTheTimer();
+}
 
-function beginQuiz() {
- questionBox.textContent = "Where is the script tag placed on the HTML page?";
-  buttonOne.textContent = "A In the head tag",
-  buttonTwo.textContent = "B Before the closing body tag",  
-  buttonThree.textContent = "C After the closing html tag", 
-  buttonFour.textContent = "D inside of a div tag"
-}; 
+// advances you to the next question
+function continueQuiz() {
+  resetOptions();
+  startQuiz(allQuestions[currentQuestionAsked])
+}
+
+// // questions
+function startQuiz(quizQuestions) {
+  questionSection.innerText = quizQuestions.question
+  quizQuestions.choices.forEach(answer=> {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add("selections")
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', checkSelection)
+    answerSection.appendChild(button)
+  })
+}
+
+function resetOptions() {
+  nextButton.classList.add('hide')
+  while(answerSection.firstChild) {
+    answerSection.remove(answerSection.firstChild)
+  }
+}
+function checkSelection(e) {
+
+}
+
+// Array of questions that will be asked and displayed
+const quizQuestions = [
+    {
+      question: "What tag defines a list item?",
+      choices: [
+        { text: "li", correct: true },
+        { text: "ul", correct: false },
+        { text: "p", correct: false }, 
+        { text: "h2", correct: false }
+      ]
+    },
+    {
+      question: "Which JavaScript element stores multiple values in a single variable?",
+      choices: [
+        { text: "functions", correct: false },
+        { text: "array", correct: true },
+        { text: "string", correct: false }, 
+        { text: "boolean", correct: false }
+      ]
+    },
+    {
+      question: "What does HTML stand for?",
+      choices: [
+        { text: "hyperlink and text markup language", correct: false },
+        { text: "hometext markup language", correct: false },
+        { text: "hypertext markup language", correct: true }, 
+        { text: "headerlink markup language", correct: false }
+      ]
+    },
+    {
+      question: "How do you insert a line break?",
+      choices: [
+        { text: "strong tag", correct: false },
+        { text: "head tag", correct: false },
+        { text: "break tag", correct: false }, 
+        { text: "br tag", correct: true }
+      ]
+    },
+    {
+      question: "Commonly used data types do not include?",
+      choices: [
+        { text: "strings", correct: false },
+        { text: "booleans", correct: false },
+        { text: "alerts", correct: true }, 
+        { text: "numbers", correct: false }
+      ]
+    }
+  ]
+
 
 // The timer is set and will stop when time is up or all questions are answered
 function startTheTimer() {
@@ -33,7 +120,5 @@ function sendMessage() {
   countDown.textContent = "Time is Up";
 }
 
-startTheTimer();
-
 // when I click the button the quiz will appear
-startBtn.addEventListener("click", beginQuiz);
+startBtn.addEventListener("click", playQuiz);
